76e04b3f4f7e1217a2e18c2db8e189d3dc2bd58d
abhisheksurela79 <abhisheksurela79@gmail.com>
2023-03-25 13:01:16 +0530

    first commit

--- APIProject/settings.py
+++ APIProject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-psd8nx_ncoi#$lb&wnr3__#sdz4c4+qr_gbiwp)&*is2d@larg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -41,6 +41,13 @@ INSTALLED_APPS = [
     'rest_framework',
     'api',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework.renderers.JSONRenderer',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
